293de52dadc5782e902a13de2c4055e96b326eaa
Shreyash-chavare <shreyashchavare04@gmail.com>
2025-04-07 09:15:19 +0530

    Forgot-password fixed

--- Frontend/src/pages/Home/components/ForgotPassword.jsx
+++ Frontend/src/pages/Home/components/ForgotPassword.jsx
@@ -102,7 +102,7 @@ const ForgotPassword = () => {
                 type="password"
                 required
                 className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Enter your current password"
+                placeholder="Enter your Newpassword"
                 value={newpass}
                 onChange={(e) => setNewpass(e.target.value)}
               />
